f7ff2456567980cb5f5f9de05b814d2e75b4632c
mldht <htdkd0511@163.com>
2020-07-19 14:45:33 +0800

    add solution for 226

---
+++ leetcode/226_invert_binary_tree/1.c
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+struct TreeNode* invertTree(struct TreeNode* root){
+
+    if(!root)
+    {
+        return NULL;
+    }
+
+    struct TreeNode *left = invertTree(root->left);
+    struct TreeNode *right = invertTree(root->right);
+    root->left = right;
+    root->right = left;
+    return root;
+}
